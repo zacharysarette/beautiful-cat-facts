902daa1f9f721ebc5ab55f03be5f6dd931f42949
zacharysarette <zacharywsarette@gmail.com>
2019-12-18 01:38:38 +0900

    Hooking up the APIs and making a basic MVP

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,53 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <app-cat-card :info="catInfo" :catPicSource="catPic"></app-cat-card>
+      <div v-if="loading">Loading...</div>
+      <v-content v-else>
+        <p> Loaded</p>
+      </v-content>
+      <div v-if="errored"><p>{{ error }}</p></div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import CatCard from './components/CatCard'
+import axios from 'axios'
+axios.defaults.timeout = 5000
 export default {
   name: 'App',
-
   components: {
-    HelloWorld
+    appCatCard: CatCard
   },
-
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      catPic: '',
+      catInfo: 'this is cat data',
+      error: null,
+      errored: false,
+      loading: true
+    }
+  },
+  mounted: async function () {
+    await axios
+      .get('https://api.thecatapi.com/v1/images/search')
+      .then(response => (this.catPic = response.data[0].url))
+      .catch(error => {
+        this.error = error
+        this.errored = true
+      })
+      .finally(() => (this.loading = false))
+    await axios
+      .get('https://catfact.ninja/fact')
+      .then(response => (this.catInfo = response.data.fact))
+      .catch(error => {
+        this.error = error
+        this.errored = true
+      })
+      .finally(() => (this.loading = false))
+  }
 }
 </script>
---
+++ src/components/CatCard.vue
@@ -0,0 +1,26 @@
+<template>
+<v-card
+    max-width="200px"
+    max-height="500px"
+>
+  <v-card-title>Cat Fact</v-card-title>
+  <v-img
+    :src="catPicSource"
+    max-width="200px"
+    max-height="200px">
+  </v-img>
+  <v-card-text>
+    {{ pic }}
+    {{ info }}
+  </v-card-text>
+</v-card>
+</template>
+
+<script>
+export default {
+  name: 'CatCard',
+  props: ['info', 'catPicSource'],
+  data: () => ({
+  })
+}
+</script>

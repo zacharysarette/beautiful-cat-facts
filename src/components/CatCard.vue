5a5337f015d2979cc527febd99e3769a7336072f
zacharysarette <zacharywsarette@gmail.com>
2019-12-19 00:30:39 +0900

    adds icons and random suit and numbers to cat fact cards

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -3,7 +3,14 @@
     max-width="200px"
     v-ripple
 >
-  <v-card-title>Cat Fact</v-card-title>
+  <v-card-title >Cat Fact
+    <v-spacer/>
+    {{ randNum2 === 1 ? 'A' : randNum2 }}
+    <v-icon v-if="randNum === 0">mdi-cards-spade</v-icon>
+    <v-icon v-if="randNum === 1">mdi-cards-heart</v-icon>
+    <v-icon v-if="randNum === 2">mdi-cards-club</v-icon>
+    <v-icon v-if="randNum === 3">mdi-cards-diamond</v-icon>
+  </v-card-title>
   <v-img
     :src="catPicSource"
     max-width="200px"
@@ -17,9 +24,21 @@
 </template>
 
 <script>
+import { mdiCardsSpade, mdiCardsClub, mdiCardsDiamond, mdiCardsHeart } from '@mdi/js'
 export default {
   name: 'CatCard',
+  created () {
+    this.randNum = Math.floor(Math.random() * 4)
+    this.randNum2 = Math.floor((Math.random() * 10) + 1)
+  },
+  mdiCardsSpade,
+  mdiCardsClub,
+  mdiCardsDiamond,
+  mdiCardsHeart,
+  randNum: 1,
+  randNum2: 1,
   props: ['info', 'catPicSource'],
+
   data: () => ({
   })
 }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuetify, { colors } from 'vuetify/lib'
+import { mdiAccount } from '@mdi/js'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdiSvg'
+    iconfont: 'mdiSvg', mdiAccount
   },
   theme: {
     dark: false,
